cb6e06962ba5b800408e021b24983217c5aebb74
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-09-08 11:44:13 +0400

    shortest word is at least 3 characters long

--- tests/config.test.js
+++ tests/config.test.js
@@ -27,3 +27,11 @@ test("config variable words searching should not end with a space", () => {
 test("config variable words should be separated by a space to be able to create an array of more than one word", () => {
     expect(configVariables.WORDS_TO_SEARCH_FOR.split(' ').length).toBeGreaterThan(1)
 });
+
+test("config variable words shortest word should be at least 3 characters long", () => {
+    const shortestWord = configVariables.WORDS_TO_SEARCH_FOR.split(' ').sort(function(a, b) {
+        return a.length - b.length;
+      })[0];
+      
+    expect(shortestWord.length).toBeGreaterThan(2)
+});
